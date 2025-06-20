209832adcf3fda728ee08f452b18836a3ca43a65
Adria <adria.jorda.m@gmail.com>
2025-06-20 23:47:05 +0200

    [add] 04 iterfaces terminado

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HousingLocation} from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
 @Component({
   selector: 'app-home',
   imports: [CommonModule, HousingLocation],
@@ -18,5 +19,15 @@ import {HousingLocation} from '../housing-location/housing-location';
   styleUrls: ['./home.css'],
 })
 export class Home {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
